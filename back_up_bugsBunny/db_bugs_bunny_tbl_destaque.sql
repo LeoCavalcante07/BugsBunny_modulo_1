e275b8e97741826d719f7d418a6f28d0522ee57f
unknown <leonardp_cavalcante.07@hotmail.com>
2018-11-04 23:16:12 -0200

    Commit com banco

---
+++ back_up_bugsBunny/db_bugs_bunny_tbl_destaque.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_bugs_bunny
+-- ------------------------------------------------------
+-- Server version	8.0.12
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_destaque`
+--
+
+DROP TABLE IF EXISTS `tbl_destaque`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tbl_destaque` (
+  `idDestaque` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(100) NOT NULL,
+  `texto` text NOT NULL,
+  `foto` varchar(100) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`idDestaque`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_destaque`
+--
+
+LOCK TABLES `tbl_destaque` WRITE;
+/*!40000 ALTER TABLE `tbl_destaque` DISABLE KEYS */;
+INSERT INTO `tbl_destaque` VALUES (4,'aaa','WJDLKJEWAFDLKEWAJFLKJEWA\r\nEAFKJHEKWHFKJHEWKJFEW\r\nEWJHFKJEWHKFHEWKDNMWD\r\nSDMSAND,NEWD\r\nWDWDW                                     ','arquivos/2acf4114d9de48c4420eade4a7150777.jpg',1),(5,'tttt','                  “Onde estou?” e “como vim parar aqui?” foram as primeiras perguntas que surgiram na mente confusa daquele jovem de aparentemente vinte anos. Era branco, com cabelos negros e olhos castanhos, vestia uma camisa branca e um short                           ','arquivos/b9658a849232cc6db833db6e71a18785.jpg',1);
+/*!40000 ALTER TABLE `tbl_destaque` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-11-04 23:14:49
